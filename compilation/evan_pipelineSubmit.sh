b388e4f2d2f99b997e3395ad0261701577e275f6
Evan Schaffer <schafferEvan@users.noreply.github.com>
2019-07-06 08:45:27 -0400

    updated submit script file management

---
+++ compilation/evan_pipelineSubmit.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+
+# --- these are the only lines to edit ------------------------------
+# -------------------------------------------------------------------
+flyNum="fly1"
+imagingDataDir="/Volumes/SCAPEdata1/finalData/2019_06_26_Nsyb_NLS6s_walk/"$flyNum"/"
+behaviorDataDir="/Volumes/SCAPEdata1/scapeBehavior/2019_06_26_Nsyb_NLS6s_walk/"
+matlabPath="/Applications/MATLAB_R2018a.app/bin/matlab"
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+bash submit_postProcessing.sh $imagingDataDir $behaviorDataDir $matlabPath $flyNum
