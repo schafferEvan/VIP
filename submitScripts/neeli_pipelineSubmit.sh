4b9d7b0662a5933f1bacd3e4b2e467e7a767f6c6
Neeli Mishra <neelimishra@gmail.com>
2019-07-06 22:40:39 -0400

    updated my submit script

--- submitScripts/neeli_pipelineSubmit.sh
+++ submitScripts/neeli_pipelineSubmit.sh
@@ -2,9 +2,10 @@
 
 # --- these are the only lines to edit ------------------------------
 # -------------------------------------------------------------------
-imagingDataDir="/Volumes/SCAPEdata1/finalData/2019_06_28_Nsyb_NLS6s_walk/fly3/"
+flyNum="fly3"
+imagingDataDir="/Volumes/SCAPEdata1/finalData/2019_06_28_Nsyb_NLS6s_walk/"$flyNum"/"
 behaviorDataDir="/Volumes/SCAPEdata1/scapeBehavior/2019_06_28_Nsyb_NLS6s_walk/"
 matlabPath="/Applications/MATLAB_R2018b.app/bin/matlab"
 # -------------------------------------------------------------------
 # -------------------------------------------------------------------
-bash submit_postProcessing.sh $imagingDataDir $behaviorDataDir $matlabPath
+bash submit_postProcessing.sh $imagingDataDir $behaviorDataDir $matlabPath $flyNum
