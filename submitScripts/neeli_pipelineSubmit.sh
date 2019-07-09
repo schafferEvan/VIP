450e8ea46711595b5b89e4be3fae56f951617a1e
Neeli <neelimishra@gmail.com>
2019-07-09 19:18:45 -0400

    updated

--- submitScripts/neeli_pipelineSubmit.sh
+++ submitScripts/neeli_pipelineSubmit.sh
@@ -2,7 +2,7 @@
 
 # --- these are the only lines to edit ------------------------------
 # -------------------------------------------------------------------
-flyNum="fly2"
+flyNum="fly3"
 imagingDataDir="/Volumes/SCAPEdata1/finalData/2019_06_28_Nsyb_NLS6s_walk/"$flyNum"/"
 behaviorDataDir="/Volumes/SCAPEdata1/scapeBehavior/2019_06_28_Nsyb_NLS6s_walk/"
 matlabPath="/Applications/MATLAB_R2018b.app/bin/matlab"
