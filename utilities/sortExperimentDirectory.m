cea94499b80e81d62242aa61a43c70d8402513c0
Neeli <neelimishra@gmail.com>
2019-07-09 17:30:01 -0400

    updated sort script to allow for more use cases

--- utilities/sortExperimentDirectory.m
+++ utilities/sortExperimentDirectory.m
@@ -1,4 +1,4 @@
-function [trials, trialOrder, runNum, runNumConvert, frameNum] = sortExperimentDirectory(experimentFolder)
+function [trials, trialOrder, runNum, runNumConvert, frameNum] = sortExperimentDirectory(experimentFolder, typeFile)
 % File sorting in experimental directory.
 
 trials = dir([experimentFolder,'*.mat']); %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%dir([experimentFolder,'*.h5']);
@@ -9,16 +9,19 @@ for j=1:length(trials)
     m = matfile([experimentFolder,trials(j).name]); % this is to check if any file is corrupted
     runLoc = strfind(trials(j).name,'run');
     underscoreLoc = strfind(trials(j).name,'_');
-    regLoc = strfind(trials(j).name,'reg');
-    if isempty(regLoc)
-        regLoc = strfind(trials(j).name,'.');
+    typeLoc = strfind(trials(j).name, typeFile);
+    underscoreInType = strfind(typeFile,'_');
+    if isempty(typeLoc)
+        typeLoc = strfind(trials(j).name,'.');
     end
     if ~isempty(runLoc)
         runNum{j} = trials(j).name(runLoc+3:underscoreLoc(end)-1);
     else
         runNum{j} = '1';
     end
-    frameNum(j) = str2double(trials(j).name(underscoreLoc(end)+1:regLoc(1)-1));
+  
+    frameNum(j) = str2double(trials(j).name(underscoreLoc(end)+1:typeLoc(1)-1));
+     
     uLocTemp = strfind(runNum{j},'_');
     numTemp = runNum{j};
     if length(uLocTemp) > 0
@@ -31,6 +34,9 @@ for j=1:length(trials)
     
 end
 
-[~,trialOrder] = sort( runNumConvert*10^(1+ceil(log10(max(frameNum)))) + frameNum, 'ascend' );
+if strcmp(typeFile, '_info') == 1
+    [~,trialOrder] = sort( runNumConvert, 'ascend' );
+else    
+    [~,trialOrder] = sort( runNumConvert*10^(1+ceil(log10(max(frameNum)))) + frameNum, 'ascend' );
 end
 
