aab0cf45bc9e5085f68fe86e1a835263f724c225
schafferEvan <schaffer.evan@gmail.com>
2019-07-10 18:41:28 -0400

    adding dlc output to plots (incomplete)

--- submitScripts/submit_visualization.sh
+++ submitScripts/submit_visualization.sh
@@ -5,6 +5,8 @@
 dataDir=$1
 saveDir=$2
 matlabPath=$3
+expID=$4
+
 
 echo $dataDir
 echo $saveDir
@@ -15,6 +17,19 @@ parentdir="$(dirname "$cwd")"
 
 # make quick temporally downsampled movie
 $matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); make_quickMovie $parentdir $dataDir $saveDir; exit"
+
+
+# make plots of traces of active cells with spatial footprints and behavior traces
+#fromGreenCC = false; 	 % use ROIs parsed from green image (false -> use red)
+#showBallVar = true;     % show motion energy of ball extracted as pix var
+#showDrink = false;      % show trace of bubble and drinking
+#showDLC = true;         % show output of deeplabcut
+fromGreenCC=0
+showBallVar=1
+showDrink=0
+showDLC=1
+$matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); plotActiveNeurons $parentdir $dataDir $expID $saveDir $fromGreenCC $showBallVar $showDrink $showDLC; exit"
+
 
 # make full movie (all frames)
 # $matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); make_rawScaled_movies $parentdir $dataDir $saveDir; exit"
