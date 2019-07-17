d4182ab0578b4e78502a95c6d1c5fca9f68d5edb
schafferEvan <schaffer.evan@gmail.com>
2019-07-17 12:09:59 -0400

    Update evan_temp.sh

--- submitScripts/evan_temp.sh
+++ submitScripts/evan_temp.sh
@@ -3,7 +3,8 @@
 # --- these are the only lines to edit ------------------------------
 # -------------------------------------------------------------------
 flyNum="fly1"
-imagingDataDir="/Volumes/SCAPEdata1/finalData/2019_06_30_Nsyb_NLS6s_walk/"$flyNum"/"
+# imagingDataDir="/Volumes/SCAPEdata1/finalData/2019_06_30_Nsyb_NLS6s_walk/"$flyNum"/"
+imagingDataDir="/Volumes/dataFast/2019_06_30_Nsyb_NLS6s_walk/2019_06_30/"$flyNum"/"
 behaviorDataDir="/Volumes/SCAPEdata1/scapeBehavior/2019_06_30_Nsyb_NLS6s_walk/"
 matlabPath="/Applications/MATLAB_R2018a.app/bin/matlab"
 # -------------------------------------------------------------------
