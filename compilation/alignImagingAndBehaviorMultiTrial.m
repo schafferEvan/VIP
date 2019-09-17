b4689bf20b1e61873968745d224de92b6bbaee4f
schafferEvan <schaffer.evan@gmail.com>
2019-09-17 19:51:57 -0400

    align for multiple behav files now up to date with pipeline

--- compilation/alignImagingAndBehaviorMultiTrial.m
+++ compilation/alignImagingAndBehaviorMultiTrial.m
@@ -11,7 +11,8 @@ addpath(genpath(codePath))
 imagingFile = [traceFolder,'F.mat']; %[traceFolder,'all.mat'];
 load(imagingFile,'trialFlag');
 behaviorDirectory = dir([traceFolder,'behavior/fly*']); %dir([baseFolder,'scapeBehavior/',experiment,'*.mat']);
-extraSkippedFrames = 0; % DEPRECATED. needs to be vector if reintegrated
+repeatedBleachBuffer = 0; % this is now handled in postprocessing
+
     
 % get file order
 tmp = zeros(size(behaviorDirectory));
@@ -34,22 +35,51 @@ for i=1:length(behaviorDirectory)
     
     % grab length of correct piece of imaging data
     imRunLength = sum(trialFlag==runIds(i));
+        
+    % ignore first 30s of beginning of experiment, but not subsequent runs
+    if i==1; bleachBuffer = 30*round(info.daq.scanRate);
+    else;    bleachBuffer = repeatedBleachBuffer*round(info.daq.scanRate);
+    end
     
-    % time
-    bleachBuffer = 30*round(info.daq.scanRate);
-    if info.daq.numberOfScans-bleachBuffer-extraSkippedFrames~=imRunLength; error('length of traces does not match expected'); end
-    timeTot = (1:info.daq.numberOfScans)/info.daq.scanRate; %1/info.daq.scanRate:1/info.daq.scanRate:info.daq.scanLength;
-    time = timeTot(bleachBuffer+extraSkippedFrames+1:end);
-    %time = time(extraSkippedFrames+1:end);
+    % check that extracted imaging data has correct number of frames
+    disp({'# scans: ',num2str(info.daq.numberOfScans);...
+        'bleachBuffer: ',num2str(bleachBuffer);...
+        'imRunLength: ',num2str(imRunLength)})
+    if info.daq.numberOfScans-bleachBuffer~=imRunLength; error('length of traces does not match expected'); end
+    
+    % populate imaging time vector
+    timeTot = (1:info.daq.numberOfScans)/info.daq.scanRate;
+    timeTmp = timeTot(bleachBuffer+1:end);
+
+    %     if info.daq.numberOfScans-bleachBuffer-extraSkippedFrames~=imRunLength; error('length of traces does not match expected'); end
+    %     timeTot = (1:info.daq.numberOfScans)/info.daq.scanRate; %1/info.daq.scanRate:1/info.daq.scanRate:info.daq.scanLength;
+    %     time = timeTot(bleachBuffer+extraSkippedFrames+1:end);
+    %     %time = time(extraSkippedFrames+1:end);
     
     % behavior
+    behRaw = load(behaviorFilename);
+    behaviorOpts.parseStruct = getBehParsing(double(behRaw.traces.isImagingOn), traceFolder);
+    
+    % check for deeplabcut output and load if available
+    dlcname = dir([traceFolder,'behavior/*DeepCut*.csv']);
+    if ~isempty(dlcname)
+        behRaw.nDLCpts = 8; disp('update dlcRead to allow for variable number of points')
+        behRaw.dlcData = dlcRead([traceFolder, 'behavior/', dlcname.name],behRaw.nDLCpts);
+    else
+        behRaw.nDLCpts = 0;
+        behRaw.dlcData = [];
+    end
+    
     behaviorOpts.basicBehav = true; %false; % true => old defaults, ok if behav is const 30fps
     behaviorOpts.info = info;
     behaviorOpts.timeTot = timeTot;
     behaviorOpts.bleachBuffer = bleachBuffer;
     
-    beh = formatBehaviorData(behaviorFilename, behaviorOpts);
-    alignedBehaviorTot = concatenateBehaviorFiles(alignedBehaviorTot, beh, time);
+    beh = formatBehaviorData(behRaw, behaviorOpts, 1); % iter=1, not i
+    disp('Behavior file added')
+    alignedBehaviorTot = concatenateBehaviorFiles(alignedBehaviorTot, beh, timeTmp);
+    %     beh = formatBehaviorData(behaviorFilename, behaviorOpts);
+    %     alignedBehaviorTot = concatenateBehaviorFiles(alignedBehaviorTot, beh, time);
 
 end
 
@@ -59,27 +89,44 @@ bmat.time = alignedBehaviorTot.timeTot;
 
 
 
-function behAligned = formatBehaviorData(behaviorFilename,behaviorOpts)
+function behAligned = formatBehaviorData(beh, behaviorOpts, iter)
 
-% takes as input a matfile made by extractBehaviorManual.m
+% takes as input a matfile made by extractBehaviorManual.m or extractBehaviorAuto.m
 
-beh = load(behaviorFilename);
-beh.time.imOn = find(beh.traces.isImagingOn,1);
-beh.time.imOff = find(beh.traces.isImagingOn,1,'last');
+beh.time.imOn = behaviorOpts.parseStruct.starts(iter);
+beh.time.imOff = behaviorOpts.parseStruct.stops(iter);
 beh.smoothing = max(round( ((beh.time.imOff-beh.time.imOn)/behaviorOpts.info.daq.scanLength)/behaviorOpts.info.daq.scanRate ),1);    % smooth by behavior frames per imaging frame
 beh.time.trueTime = (1:(beh.time.imOff-beh.time.imOn+1))/(beh.time.imOff-beh.time.imOn+1)*behaviorOpts.info.daq.scanLength;            % fixed?
 
+% smooth, align, and trim traces from simple behavior extraction
 beh.traces.legVarSmooth = smooth(beh.traces.legVar(beh.time.imOn:beh.time.imOff), beh.smoothing)';
 beh.traces.stimSmooth = beh.traces.isStimOn(beh.time.imOn:beh.time.imOff);
 beh.traces.drinkSmooth = beh.traces.isDrinking(beh.time.imOn:beh.time.imOff);
 
-beh.traces.legVarSmoothAligned = interp1(beh.time.trueTime, beh.traces.legVarSmooth, behaviorOpts.timeTot);    % interpolate to align time
-beh.traces.stimSmoothAligned = interp1(beh.time.trueTime, beh.traces.stimSmooth, behaviorOpts.timeTot);    % interpolate to align time
-beh.traces.drinkSmoothAligned = interp1(beh.time.trueTime, beh.traces.drinkSmooth, behaviorOpts.timeTot);    % interpolate to align time
+beh.traces.legVarSmoothAligned = interp1(beh.time.trueTime, beh.traces.legVarSmooth, behaviorOpts.timeTot,'nearest','extrap');    % interpolate to align time
+beh.traces.stimSmoothAligned = interp1(beh.time.trueTime, beh.traces.stimSmooth, behaviorOpts.timeTot,'nearest','extrap');    % interpolate to align time
+beh.traces.drinkSmoothAligned = interp1(beh.time.trueTime, beh.traces.drinkSmooth, behaviorOpts.timeTot,'nearest','extrap');    % interpolate to align time
 
 behAligned.legVar = beh.traces.legVarSmoothAligned(behaviorOpts.bleachBuffer+1:end);
 behAligned.stim = beh.traces.stimSmoothAligned(behaviorOpts.bleachBuffer+1:end);
 behAligned.drink = beh.traces.drinkSmoothAligned(behaviorOpts.bleachBuffer+1:end);
+
+
+% smooth, align, and trim traces from deeplabcut behavior extraction
+beh.dlcAligned.tmp.smooth = zeros(length(beh.time.trueTime), beh.nDLCpts*3);
+beh.dlcAligned.tmp.aligned = zeros(length(behaviorOpts.timeTot), beh.nDLCpts*3);
+beh.dlcAligned.data = zeros(length(behaviorOpts.timeTot)-behaviorOpts.bleachBuffer, beh.nDLCpts*3);
+if beh.nDLCpts>0
+    nms = beh.dlcData.Properties.VariableNames;
+end
+
+for j=1:beh.nDLCpts*3 % factor of 3 for x, y, and likelihood   
+    dataChunk = beh.dlcData.(nms{j})(beh.time.imOn:beh.time.imOff);
+    beh.dlcAligned.tmp.smooth(:,j) = smooth(dataChunk, beh.smoothing)';
+    beh.dlcAligned.tmp.aligned(:,j) = interp1(beh.time.trueTime, beh.dlcAligned.tmp.smooth(:,j), behaviorOpts.timeTot,'nearest','extrap');    % interpolate to align time
+    beh.dlcAligned.data(:,j) = beh.dlcAligned.tmp.aligned(behaviorOpts.bleachBuffer+1:end,j);
+end
+behAligned.dlcAligned.data = beh.dlcAligned.data;
 
 
 
@@ -101,4 +148,33 @@ else
 end
 
 
-
+function parseStruct = getBehParsing(isImagingOn, traceFolder)
+% converts a noisy measure of the indicator LED turning on/off in the
+% behavior video into a binary vector by denoising, fitting a gaussian
+% mixture model, then further smoothing the posterior to prevent flicker
+imSm = TVL1denoise(double(isImagingOn), 0.1, 100);
+if size(imSm,1)==1; imSm=imSm'; end
+GMModel = fitgmdist(imSm,2);
+P = posterior(GMModel,imSm);
+[~,onIdx] = max(GMModel.mu);
+[~,offIdx] = min(GMModel.mu);
+gOn=TVL1denoise(P(:,onIdx), 0.1, 100); 
+gOff=TVL1denoise(P(:,offIdx), 0.1, 100);
+parseVec = gOn>gOff; % where posterior is higher for "on" distribution
+dP = diff(parseVec);
+starts = find(dP>0);
+stops = find(dP<0);
+parseStruct = struct('starts',starts,'stops',stops);
+% if (min(diff(starts))<100) || (min(diff(stops))<100)
+if (length(starts)>1) || (length(stops)>1)
+    error('INDICATOR MEASURE IS FLICKERING. PARSING FAILURE');
+elseif length(starts)~=length(stops)
+    mkdir([traceFolder, 'crashReport']);
+    crashMat = matfile([traceFolder, 'crashReport/','parseStruct.mat'],'Writable',true);
+    crashMat.parseStruct = parseStruct;
+    h = figure; 
+    plot(isImagingOn);
+    savefig(h, [traceFolder, 'crashReport/','isImagingOn.fig']);
+    % save parseStruct and trace as fig
+    error('NUMBER OF STARTS AND STOPS DO NOT MATCH. PARSING FAILURE');
+end
