25babdc5ede6cc158f73f297e246e5f138bfac61
schafferEvan <schaffer.evan@gmail.com>
2019-10-15 11:19:22 -0400

    separated watershed and sumImage steps

--- imaging/postProcessing/compile_sumImage.m
+++ imaging/postProcessing/compile_sumImage.m
@@ -89,12 +89,3 @@ end
 
 mkdir([experimentFolder,'Yproj'])
 save([experimentFolder,'Yproj/Ysum.mat'],'Ysum','Rsum')
-
-[~,Ycc] = cell_segment_watershed(Ysum);
-% [im_bw_out,Ycc,regionProps] = segmentHessian_SCAPE_new(Ysum);
-if ~isempty(Rsum)
-    [~,Rcc,regionProps] = cell_segment_watershed(Rsum);
-else
-    Rcc = [];
-end
-save([experimentFolder,'Yproj/cc.mat'],'Ycc','Rcc','regionProps')
