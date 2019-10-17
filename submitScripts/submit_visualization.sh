dd73e4f4571042de85edd429d99a6faa7bc6cc9e
Neeli <neelimishra@gmail.com>
2019-10-17 16:43:31 -0400

    updated script to have lines to toggle for sparse lines

--- submitScripts/submit_visualization.sh
+++ submitScripts/submit_visualization.sh
@@ -19,24 +19,28 @@ parentdir="$(dirname "$cwd")"
 
 # make quick temporally downsampled movie
 $matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); make_quickMovie $parentdir $dataDir $saveDir; exit"
+# if making a video of sparse lines, toggle below
+# $matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); make_quickMovie_twoColor $parentdir $dataDir $saveDir; exit"
 
 
-# # make plots of traces of active cells with spatial footprints and behavior traces
-# #fromGreenCC = false; 	 % use ROIs parsed from green image (false -> use red)
-# #showBallVar = true;     % show motion energy of ball extracted as pix var
-# #showDrink = false;      % show trace of bubble and drinking
-# #showDLC = true;         % show output of deeplabcut
-# fromGreenCC=0
-# showBallVar=1
-# showDrink=0
-# showDLC=1
-# $matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); plotActiveNeurons $parentdir $traceDir $expID $saveDir $fromGreenCC $showBallVar $showDrink $showDLC; exit"
-# fromGreenCC=1
-# $matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); plotActiveNeurons $parentdir $traceDir $expID $saveDir $fromGreenCC $showBallVar $showDrink $showDLC; exit"
+# make plots of traces of active cells with spatial footprints and behavior traces
+#fromGreenCC = false; 	 % use ROIs parsed from green image (false -> use red)
+#showBallVar = true;     % show motion energy of ball extracted as pix var
+#showDrink = false;      % show trace of bubble and drinking
+#showDLC = true;         % show output of deeplabcut
+fromGreenCC=0
+showBallVar=1
+showDrink=0
+showDLC=1
+$matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); plotActiveNeurons $parentdir $traceDir $expID $saveDir $fromGreenCC $showBallVar $showDrink $showDLC; exit"
+# # fromGreenCC=1
+# # $matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); plotActiveNeurons $parentdir $traceDir $expID $saveDir $fromGreenCC $showBallVar $showDrink $showDLC; exit"
 
 
-# make full movie (all frames)
-# $matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); make_rawScaled_movies $parentdir $dataDir $saveDir; exit"
+#make full movie (all frames)
+$matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); make_rawScaled_movies $parentdir $dataDir $saveDir; exit"
+# if making a video of sparse lines, toggle below
+$matlabPath -nodisplay -nodesktop -r "cd('../visualization/'); make_rawScaled_movies_twoColor $parentdir $dataDir $saveDir; exit"
 
 # make component movie
 # (work in progress)
