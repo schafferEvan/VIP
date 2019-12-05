216f4bbcf8b2fadd4af0e56a69074bc4f078d034
Neeli <neelimishra@gmail.com>
2019-12-05 15:59:20 -0500

    updated to check for two types of corrupted files

--- utilities/checkCorruptedMatfiles.m
+++ utilities/checkCorruptedMatfiles.m
@@ -1,19 +1,26 @@
-
 function checkCorruptedMatfiles(codePath, experimentFolder)
 % checks for any corrupted matfiles in directory
-
+ 
 addpath(genpath(codePath))
 trials = sortExperimentDirectory(experimentFolder,'reg');
-
+ 
 %% load in registered data and find ROIs
 for i=1:length(trials)
     trialPath = [experimentFolder,trials(i).name];
-
+ 
     try
         m = matfile(trialPath);
     catch
         %warning([trials(i).name, ' is a corrupted file.'])
-        disp([trials(i).name])
-    end    
-    
+        disp([trials(i).name, ' cannot be turned into a matfile.'])
+    end   
+     
+    try
+        a = load(trialPath);
+    catch
+        %warning([trials(i).name, ' is a corrupted file.'])
+        disp([trials(i).name, ' cannot be loaded.'])
+    end  
+     
+    clear a m
 end
