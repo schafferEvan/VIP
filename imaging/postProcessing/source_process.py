6d0adf5edef4eb7fdec3c559fdc72ceb9d811d16
schafferEvan <schafferEvan@users.noreply.github.com>
2020-07-28 13:25:31 -0400

    dYY and dRR to npz's

--- imaging/postProcessing/source_process.py
+++ imaging/postProcessing/source_process.py
@@ -443,7 +443,7 @@ class scape:
             io.savemat(self.baseFolder+filename+'_Agood.mat',{'goodIds':self.goodIds, 'A':self.good.A, 'dims':self.raw.dims, 'centroids':self.raw.centroids})
 
         np.savez( self.baseFolder+filename+'.npz', time=self.good.time, trialFlag=self.good.trialFlag,
-                dFF=self.dOO, ball=self.good.ball, dlc=self.good.dlc, beh_labels=self.good.beh_labels, 
+                dFF=self.dOO, dYY=self.dYY, dRR=self.dRR, ball=self.good.ball, dlc=self.good.dlc, beh_labels=self.good.beh_labels, 
                 stim=self.good.stim, drink=self.good.drink,
                 dims=self.raw.dims, dims_in_um=self.raw.dims_in_um, im=self.raw.im, 
                 scanRate=self.raw.scanRate, redTh=self.redTh, grnTh=self.grnTh, aligned_centroids=[], PIDdata=self.PIDdata) 
