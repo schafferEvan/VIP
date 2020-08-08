65514aeff9d0c41911ce9ba78b6e0dc6f61d8765
schafferEvan <schafferEvan@users.noreply.github.com>
2020-08-07 21:52:42 -0400

    output goodIds to track fraction of ROIs rejected

--- imaging/postProcessing/source_process.py
+++ imaging/postProcessing/source_process.py
@@ -444,7 +444,7 @@ class scape:
 
         np.savez( self.baseFolder+filename+'.npz', time=self.good.time, trialFlag=self.good.trialFlag,
                 dFF=self.dOO, dYY=self.dYY, dRR=self.dRR, ball=self.good.ball, dlc=self.good.dlc, beh_labels=self.good.beh_labels, 
-                stim=self.good.stim, drink=self.good.drink,
+                stim=self.good.stim, drink=self.good.drink, goodIds=self.goodIds,
                 dims=self.raw.dims, dims_in_um=self.raw.dims_in_um, im=self.raw.im, 
                 scanRate=self.raw.scanRate, redTh=self.redTh, grnTh=self.grnTh, aligned_centroids=[], PIDdata=self.PIDdata) 
         sparse.save_npz(self.baseFolder+filename+'_A.npz', self.good.A)
